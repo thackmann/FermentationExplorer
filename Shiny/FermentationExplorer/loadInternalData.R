055db8be94f6ad1c802cbd83bf3fb88658f68f53
Timothy Hackmann <34034632+thackmann@users.noreply.github.com>
2024-09-06 15:03:29 -0700

    Update loadInternalData.R

--- Shiny/FermentationExplorer/loadInternalData.R
+++ Shiny/FermentationExplorer/loadInternalData.R
@@ -1,9 +1,15 @@
+# Load Internal Data for Shiny App
+# This script loads files and objects used by the app.  These are
+# files or objects used by multiple modules.  
+# Author: Timothy Hackmann
+# Date: 6 September 2024
+
 data_fp = "data/database.csv"
 raw_data = read.csv(data_fp)
 raw_data[is.na(raw_data)] = "NA"
 
-data_fp = "data/gene_functions_database.csv"
-gene_functions = read.csv(data_fp)
+data_fp = "data/database_clean.csv"
+clean_data = read.csv(data_fp, check.names=FALSE)
 
 data_fp = "data/gene_functions_e_coli.csv"
 gene_functions_e_coli = read.csv(data_fp)
@@ -18,10 +24,13 @@ data_fp = "data/gene_functions_RUG.csv"
 gene_functions_RUG = read.csv(data_fp)
 
 data_fp = "data/reference_reactions_glucose_fermentation.csv"
-reference_reactions = read.csv(data_fp)
+reference_reactions_glucose_fermentation = read.csv(data_fp)
 
 data_fp = "data/reference_reactions_fructose_fermentation.csv"
 reference_reactions_fructose_fermentation = read.csv(data_fp)
+
+data_fp = "data/reference_reactions_methanogenesis.csv"
+reference_reactions_methanogenesis = read.csv(data_fp)
 
 data_fp = "data/taxa_uncharacterized.csv"
 taxa_uncharacterized = read.csv(data_fp)
